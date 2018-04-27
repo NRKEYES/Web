5aff5bf6f768d1619caddf0845379da0880086aa
Nicholas R. Keyes <nkeyes@unm.edu>
2018-04-27 00:06:15 -0600

    Got stacking working. Next is energetics fixing.

--- BarGraph.js
+++ BarGraph.js
@@ -3,25 +3,29 @@ The goal here is to load energies from the JSON file
 
 */
 
+
+
 var duration = 5000;
 
-var barHeight = 20,
-    barWidth = 100
+var barHeight = 16,
+    barWidth =  100;
 
 
-var width = 800,
-    height = 600;
+var height = window.innerHeight- 200
+    width = window.innerWidth - 60;
 
-var x = d3.scaleLinear()
+var y = d3.scaleLinear()
   .domain([0,-10000])
   .range([0, height])
 
 
 
 
+
+
 var chart = d3.select('.chart')
-  .attr("width", width)
-  .attr("height", height);
+  .attr("width", width-10)
+  .attr("height", height-10);
 
 
 
@@ -39,23 +43,31 @@ d3.json("GoodStuff.json", function(error, data) {
   data.forEach(function(d) {
     d.Atoms = d.Atoms;
     d.Energy = +d.Energy;
+    d.Key = +d.Key;
   });
 
 
+  var keys = []
   console.log(data.length);
   //Stacking
-  console.log(typeof data);
   for (var i = 0; i<data.length; i++){
-    console.log(data[i].Key);
-
-
+    value = data[i].Key;
+    if(!keys.includes(value)){
+      keys.push(value);
+    }
   }
+  console.log(keys);
+  var spacing = width/keys.length;
+
+  var x = d3.scaleOrdinal()
+    .domain(keys)
+    .range(math.range(0,width, spacing)._data); // the underscore data helps to get the actual array
 
 
   var bars = chart.selectAll("g")
       .data(data)
     .enter().append("g")
-      .attr("transform", function(d, i) { return "translate("+ i * width/data.length + ", 0)"; });
+      .attr("transform", function(d, i) { return "translate("+ x(d.Key) + ", 0)"; });
 
 
     bars.append("rect")
@@ -65,7 +77,7 @@ d3.json("GoodStuff.json", function(error, data) {
       .attr("width", (barWidth))
       .attr("height", (barHeight) - 1)
     .transition()
-      .attr("y", function(d) {return x(d.Energy);})
+      .attr("y", function(d) {return y(d.Energy);})
       .duration(duration);
 
     bars.append("text")
@@ -74,6 +86,6 @@ d3.json("GoodStuff.json", function(error, data) {
         .attr("dy", ".35em")
         .text(function(d) { return d.Key;})
       .transition()
-        .attr("y", function(d) { return x(d.Energy)+barHeight/2;})
+        .attr("y", function(d) { return y(d.Energy)+barHeight/2;})
         .duration(duration);
 });
