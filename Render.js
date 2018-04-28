8c04ffd8030f6b2a5a933d2fc47b12060fb5fb66
Nicholas R. Keyes <nkeyes@unm.edu>
2018-04-27 23:40:52 -0600

    DJ, spin that shit.

--- Render.js
+++ Render.js
@@ -2,6 +2,11 @@ let width_for_3d = 300;
 let height_for_3d = 300;
 
 
+let colors = {1:"0x73BFB8",
+              6:"0x2364AA",
+              79:"0xEA7317"}
+
+
 let renderer = new THREE.WebGLRenderer({alpha: true, antialiasing: true});
 renderer.setClearColor( 0x000000, 0 );
 renderer.domElement.style.position = 'absolute';
@@ -21,8 +26,6 @@ pointLight.position.z = 20
 scene.add(pointLight);
 
 
-
-
 container = document.getElementById('3d');
 container.appendChild(renderer.domElement);
 
@@ -30,27 +33,28 @@ container.appendChild(renderer.domElement);
 var spheres = [];
 
 function render(local_x,local_y,atoms, coords){
-  console.log(coords)
-  console.log(coords[0][0])
-
 
   for(var i=0; i< coords.length; i++){
-    console.log(coords[i][0]);
-    let material = new THREE.MeshLambertMaterial({ color: 0xf25346});
-    let geometry = new THREE.SphereGeometry( math.log10(atoms[i]), 20,20 );
+    console.log(colors[atoms[i]]);
+    let material = new THREE.MeshLambertMaterial({colors:colors[atoms[i]]});
+    let geometry = new THREE.SphereGeometry( math.log10(atoms[i]*.5), 20,20 );
     let sphere = new THREE.Mesh( geometry, material);
     sphere.position.set(coords[i][0],coords[i][1],coords[i][2]);
     spheres.push(sphere);
     scene.add(sphere);
   };
 
-
-
-
-
+  let theta = 0;
   function animate() {
     requestAnimationFrame( animate );
     renderer.render(scene, camera);
-  };
+
+    camera.position.x = 20*math.cos(theta);
+    camera.position.y = 20*math.sin(theta);
+    camera.position.z = 20*math.sin(theta);
+    theta+= .01;
+
+    camera.lookAt(scene.position);
+  }
   animate();
 }
